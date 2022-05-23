715f7e2047552bd2b9626cd092ed6fc83e4aa511
AlejandroCN7 <alejandroac79@gmail.com>
2022-05-23 12:07:32 +0300

    Updated rewards

--- sinergym/utils/rewards.py
+++ sinergym/utils/rewards.py
@@ -3,7 +3,7 @@
 
 from datetime import datetime
 from math import exp
-from typing import Any, Dict, List, Tuple, Union
+from typing import Dict, List, Tuple, Union
 
 from gym import Env
 
@@ -77,12 +77,12 @@ class LinearReward(BaseReward):
         self.summer_start_date = datetime(self.year, 6, 1)
         self.summer_final_date = datetime(self.year, 9, 30)
 
-    def __call__(self) -> Tuple[float, Dict[str, Any]]:
+    def __call__(self):
         """
         Calculate the reward function.
 
         Returns:
-            Tuple[float, Dict[str, Any]]: Reward value and dictionary with their individual components.
+            Tuple[float, Dict[str, float]]: Reward value and dictionary with their individual components.
         """
         # Current observation
         obs_dict = self.env.obs_dict.copy()
@@ -107,15 +107,8 @@ class LinearReward(BaseReward):
 
         return reward, reward_terms
 
-    def _get_comfort(self,
-                     obs_dict: Dict[str,
-                                    Any]) -> Tuple[float,
-                                                   List[float]]:
-        """Calculate the comfort term of the reward.
-
-        Returns:
-            Tuple[float, List[float]]: comfort penalty and List with temperatures used.
-        """
+    def _get_comfort(self, obs_dict):
+        """Calculate the comfort term of the reward."""
 
         month = obs_dict['month']
         day = obs_dict['day']
@@ -176,15 +169,8 @@ class ExpReward(LinearReward):
             lambda_temperature
         )
 
-    def _get_comfort(self,
-                     obs_dict: Dict[str,
-                                    Any]) -> Tuple[float,
-                                                   List[float]]:
-        """Calculate the comfort term of the reward.
-
-        Returns:
-            Tuple[float, List[float]]: comfort penalty and List with temperatures used.
-        """
+    def _get_comfort(self, obs_dict):
+        """"""
 
         month = obs_dict['month']
         day = obs_dict['day']
@@ -248,12 +234,8 @@ class HourlyLinearReward(LinearReward):
         # Reward parameters
         self.range_comfort_hours = range_comfort_hours
 
-    def __call__(self) -> Tuple[float, Dict[str, Any]]:
-        """Calculate the reward function.
-
-        Returns:
-            Tuple[float, Dict[str, Any]]: Reward and dict with reward terms.
-            """
+    def __call__(self):
+        """Calculate the reward function."""
         # Current observation
         obs_dict = self.env.obs_dict.copy()
 
@@ -282,3 +264,227 @@ class HourlyLinearReward(LinearReward):
         }
 
         return reward, reward_terms
+
+
+class OccupancyBasedReward(BaseReward):
+
+    def __init__(
+        self,
+        env: Env,
+        temperature_variable: Union[str, list],
+        energy_variable: str,
+        range_comfort_winter: tuple,
+        range_comfort_summer: tuple,
+        energy_weight: float = 0.5,
+        lambda_energy: float = 1e-4,
+        lambda_temperature: float = 1.0
+    ):
+        """
+        Occupancy based reward function.
+
+        It considers the energy consumption and the absolute difference to temperature comfort.
+
+        .. math::
+            R = - W * lambda_E * power - (1 - W) * lambda_T * (max(T - T_{low}, 0) + max(T_{up} - T, 0))
+
+        Args:
+            env (Env): Gym environment.
+            temperature_variable (Union[str, list]): Name(s) of the temperature variable(s).
+            energy_variable (str): Name of the energy/power variable.
+            range_comfort_winter (tuple): Temperature comfort range for cold season. Depends on environment you are using.
+            range_comfort_summer (tuple): Temperature comfort range fot hot season. Depends on environment you are using.
+            energy_weight (float, optional): Weight given to the energy term. Defaults to 0.5.
+            lambda_energy (float, optional): Constant for removing dimensions from power(1/W). Defaults to 1e-4.
+            lambda_temperature (float, optional): Constant for removing dimensions from temperature(1/C). Defaults to 1.0.
+        """
+
+        super(OccupancyBasedReward, self).__init__(env)
+
+        # Name of the variables
+        self.temp_name = temperature_variable
+        self.energy_name = energy_variable
+
+        # Reward parameters
+        self.range_comfort_winter = range_comfort_winter
+        self.range_comfort_summer = range_comfort_summer
+        self.W_energy = energy_weight
+        self.lambda_energy = lambda_energy
+        self.lambda_temp = lambda_temperature
+
+        # Periods
+        self.summer_start_date = datetime(self.year, 6, 1)
+        self.summer_final_date = datetime(self.year, 9, 30)
+
+    def __call__(self):
+        """
+        Calculate the reward function.
+
+        Returns:
+            Tuple[float, Dict[str, float]]: Reward value and dictionary with their individual components.
+        """
+        # Current observation
+        obs_dict = self.env.obs_dict.copy()
+
+        # Energy term
+        reward_energy = - self.lambda_energy * obs_dict[self.energy_name]
+
+        # Comfort
+        comfort, temps = self._get_comfort(obs_dict)
+        reward_comfort = - self.lambda_temp * comfort
+
+        # Determine energy weight depending on the occupancy
+        if obs_dict['Zone People Occupant Count (SPACE1-1)'] == 0:
+            weight_energy = 1
+        else:
+            weight_energy = self.W_energy
+
+        # Weighted sum of both terms
+        reward = weight_energy * reward_energy + \
+            (1.0 - weight_energy) * reward_comfort
+
+        reward_terms = {
+            'reward_energy': reward_energy,
+            'total_energy': obs_dict[self.energy_name],
+            'reward_comfort': reward_comfort,
+            'temperatures': temps
+        }
+
+        return reward, reward_terms
+
+    def _get_comfort(self, obs_dict):
+        """Calculate the comfort term of the reward."""
+
+        month = obs_dict['month']
+        day = obs_dict['day']
+        current_dt = datetime(self.year, month, day)
+
+        if current_dt >= self.summer_start_date and current_dt <= self.summer_final_date:
+            temp_range = self.range_comfort_summer
+        else:
+            temp_range = self.range_comfort_winter
+
+        temps = [v for k, v in obs_dict.items() if k in self.temp_name]
+        comfort = 0.0
+
+        # Comfort (penalty) will be 0 if occupancy is 0
+        if obs_dict['Zone People Occupant Count (SPACE1-1)'] > 0:
+            for T in temps:
+                if T < temp_range[0] or T > temp_range[1]:
+                    comfort += min(abs(temp_range[0] - T), abs(T - temp_range[1]))
+
+        return comfort, temps
+
+
+
+class OccupancyBasedPropReward(BaseReward):
+
+    def __init__(
+        self,
+        env: Env,
+        temperature_variable: Union[str, list],
+        energy_variable: str,
+        range_comfort_winter: tuple,
+        range_comfort_summer: tuple,
+        energy_weight: float = 0.5,
+        lambda_energy: float = 1e-4,
+        lambda_temperature: float = 1.0
+    ):
+        """
+        Occupancy based proportional reward function.
+
+        It considers the energy consumption and the absolute difference to temperature comfort.
+
+        .. math::
+            R = - W * lambda_E * power - (1 - W) * lambda_T * (max(T - T_{low}, 0) + max(T_{up} - T, 0))
+
+        Args:
+            env (Env): Gym environment.
+            temperature_variable (Union[str, list]): Name(s) of the temperature variable(s).
+            energy_variable (str): Name of the energy/power variable.
+            range_comfort_winter (tuple): Temperature comfort range for cold season. Depends on environment you are using.
+            range_comfort_summer (tuple): Temperature comfort range fot hot season. Depends on environment you are using.
+            energy_weight (float, optional): Weight given to the energy term. Defaults to 0.5.
+            lambda_energy (float, optional): Constant for removing dimensions from power(1/W). Defaults to 1e-4.
+            lambda_temperature (float, optional): Constant for removing dimensions from temperature(1/C). Defaults to 1.0.
+        """
+
+        super(OccupancyBasedPropReward, self).__init__(env)
+
+        # Name of the variables
+        self.temp_name = temperature_variable
+        self.energy_name = energy_variable
+
+        # Reward parameters
+        self.range_comfort_winter = range_comfort_winter
+        self.range_comfort_summer = range_comfort_summer
+        self.W_energy = energy_weight
+        self.lambda_energy = lambda_energy
+        self.lambda_temp = lambda_temperature
+
+        # Periods
+        self.summer_start_date = datetime(self.year, 6, 1)
+        self.summer_final_date = datetime(self.year, 9, 30)
+
+        # Max occupancy
+        self.MAX_OCCUPANCY = 11
+
+    def __call__(self):
+        """
+        Calculate the reward function.
+
+        Returns:
+            Tuple[float, Dict[str, float]]: Reward value and dictionary with their individual components.
+        """
+        # Current observation
+        obs_dict = self.env.obs_dict.copy()
+
+        # Energy term
+        reward_energy = - self.lambda_energy * obs_dict[self.energy_name]
+
+        # Comfort
+        comfort, temps = self._get_comfort(obs_dict)
+        reward_comfort = - self.lambda_temp * comfort
+
+        # Determine energy weight depending on the occupancy (in a proportional way)
+        weight_energy = self.W_energy + (1 - self.W_energy) * (1 - obs_dict['Zone People Occupant Count (SPACE1-1)'] / self.MAX_OCCUPANCY)
+
+        # Weighted sum of both terms
+        reward = weight_energy * reward_energy + \
+            (1.0 - weight_energy) * reward_comfort
+
+        reward_terms = {
+            'reward_energy': reward_energy,
+            'total_energy': obs_dict[self.energy_name],
+            'reward_comfort': reward_comfort,
+            'temperatures': temps
+        }
+
+        return reward, reward_terms
+
+    def _get_comfort(self, obs_dict):
+        """Calculate the comfort term of the reward."""
+
+        month = obs_dict['month']
+        day = obs_dict['day']
+        current_dt = datetime(self.year, month, day)
+
+        if current_dt >= self.summer_start_date and current_dt <= self.summer_final_date:
+            temp_range = self.range_comfort_summer
+        else:
+            temp_range = self.range_comfort_winter
+
+        temps = [v for k, v in obs_dict.items() if k in self.temp_name]
+        comfort = 0.0
+
+        # Comfort (penalty) will be 0 if occupancy is 0
+        if obs_dict['Zone People Occupant Count (SPACE1-1)'] > 0:
+            for T in temps:
+                if T < temp_range[0] or T > temp_range[1]:
+                    comfort += min(abs(temp_range[0] - T), abs(T - temp_range[1]))
+
+#        comfort = comfort * obs_dict['Zone People Occupant Count (SPACE1-1)'] / self.MAX_OCCUPANCY
+
+        return comfort, temps
+
+
+
